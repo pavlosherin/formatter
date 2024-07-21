b55f8cf793d791c8fea2d8a21575d576f07a93d5
Pavlo Sherin <sherin.pavel@gmail.com>
2024-07-22 01:15:13 +0200

    add formatter service

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import moment from 'moment/moment';
+
 export class NumberFormatter {
   private _parseMask(mask: string): [_decimalSeparator: string, _thousandsSeparator: string, _isNumericMask: boolean, _isWithoutDecimal: boolean] {
     const _thousandsSeparator = mask
@@ -130,4 +132,131 @@ export class DateFormatter {
     { momentFormatPart: 'mm', primengFormatPart: 'mm' },
     { momentFormatPart: 'ss', primengFormatPart: 'ss' },
   ];
+}
+
+export class FormatterService {
+
+  set moment(moment: any) {
+    this._moment = moment;
+  }
+  private _moment?: any;
+
+  private _numberFormatter: NumberFormatter = new NumberFormatter();
+
+  formatNumber(number: number, format: string): string | number {
+    return this._numberFormatter.formatNumber(number, format);
+  }
+
+  /**
+   * According to MomentJs patterns
+   * @param lang
+   */
+  getBaseDateFormat(lang: string): string {
+    let res = '';
+    const dateParts = new Intl.DateTimeFormat(lang).formatToParts();
+    dateParts.forEach(part => {
+      switch (part.type) {
+        case 'day':
+          res += part.value.replaceAll(/[0-9]/g, 'D');
+          break;
+        case 'month':
+          res += part.value.replaceAll(/[0-9]/g, 'M');
+          break;
+        case 'year':
+          res += part.value.replaceAll(/[0-9]/g, 'Y');
+          break;
+        case 'literal':
+          res += part.value;
+          break;
+        default:
+          break;
+      }
+    });
+    return res;
+  }
+
+  convertFormattedNumberToNumber(
+    formattedNumber: string | number,
+    mask: string | undefined
+  ): number {
+    if (mask) {
+      return this._numberFormatter.convertToNumber(formattedNumber, mask);
+    }
+    return parseFloat(<string>formattedNumber);
+  }
+
+  /**
+   * Use format service to format number by mask
+   * @param value Any value, mask is applied only to number
+   * @param entityFormatMask
+   * @private
+   */
+  private _formatNumberByMask(
+    value: unknown | number,
+    entityFormatMask?: string
+  ): string | number | unknown {
+    if (typeof value !== 'number') {
+      value = parseInt(value as string, 10);
+    }
+    if (entityFormatMask) {
+      return this.formatNumber(<number>value, entityFormatMask);
+    }
+    return value;
+  }
+
+  /**
+   * Format a value based on the given format and data type.
+   * @returns The formatted value as a string.
+   * @param params
+   */
+  formatValue(params: {
+    columnType?: 'number' | 'datetime' | 'selection' | 'string' | undefined,
+    prefix?: string,
+    suffix?: string,
+    mask?: string,
+    value?: string | number | Date | null | undefined,
+    enumValues?: any[],
+  }): string {
+    // Check if the value is null or undefined, and return an empty string if so.
+    if (params.value === null || params.value === undefined) {
+      return '';
+    }
+
+    // Ensure value is of type string, number, or Date.
+    params.value = <string | number | Date>params.value;
+
+    // Get prefix and suffix from the column entity format, default to empty strings if not defined.
+    const prefix = params?.prefix ?? '';
+    const suffix = params?.suffix ?? '';
+
+    let formattedValue = '';
+
+    // Determine how to format the value based on its data type.
+    switch (params.columnType) {
+      case 'number':
+        // Format numbers using the specified mask.
+        formattedValue = <string>this._formatNumberByMask(params.value, params.mask);
+        break;
+      case 'datetime':
+        // Format date and time values using the specified mask or default format.
+        formattedValue = this._moment(new Date(params.value)).format(
+          params?.mask ?? this._moment.defaultFormat
+        );
+        break;
+      case 'selection':
+        // Format selection values by finding the corresponding name from enumValues.
+        formattedValue =
+          params?.enumValues?.find((_enum: { value: any }) => _enum.value === params.value)
+            ?.name ?? <string>params.value;
+        break;
+      case 'string':
+      default:
+        // For string data types or if no data type is specified, keep the value as is.
+        formattedValue = <string>params.value;
+        break;
+    }
+
+    // Combine the prefix, formatted value, and suffix to create the final formatted string.
+    return [prefix, formattedValue, suffix].join(' ').trim();
+  }
 }
